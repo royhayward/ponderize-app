dae565e3e91b4b8b0bafd08c12b45b42a4da6875
Roy Hayward <roy@ponderize.me>
2025-03-12 09:25:41 -0600

    adding api environment variables and .env file.

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Scripture } from '../types/scripture';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001/api',
   headers: {
     'Content-Type': 'application/json',
   },
